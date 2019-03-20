05303f509575fc7edf2a9fdf60a632f5d5804bcb
Cody Sommer <cody.sommer@thomsonreuters.com>
2019-03-20 09:00:18 -0400

    Preserve column order

--- src/main/java/domain/CountyData.java
+++ src/main/java/domain/CountyData.java
@@ -1,14 +1,14 @@
 package domain;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class CountyData {
     private String geoId;
     private String label;
     //tableId, <columnId, <description, value>>
     //eg: "DP03_HC03_VC96",<"description", "1.0">
-    private Map<String, DataPoint> dataPoints = new TreeMap<>();
+    private Map<String, DataPoint> dataPoints = new LinkedHashMap<>();
 
     public String getGeoId() {
         return geoId;
